2e229c8904d3420a08a4e93653df055ad701b666
Giuse Petroso <g.petroso@gmail.com>
2020-06-25 23:52:31 +0200

    its working

--- AsyncPulse.cpp
+++ AsyncPulse.cpp
@@ -6,7 +6,7 @@
 #include "Arduino.h"
 #include "AsyncPulse.h"
 
-AsyncPulse::AsyncPulse(int timeOn, int timeOff);
+AsyncPulse::AsyncPulse(int timeOn, int timeOff)
 {
   _state = 1;
   _lastState = 0;
@@ -18,27 +18,37 @@ AsyncPulse::AsyncPulse(int timeOn, int timeOff);
 
 int AsyncPulse::get()
 {
-  // state rising edge
-  bool risingEdge = _state == 1 && _lastState == 0;
-  if (risingEdge)
-    _countOn = millis();
-
-  // state falling edge
-  bool fallingEdge = _state == 0 && _lastState == 1;
-  if (fallingEdge)
-    _countOff = millis();
-
-  if (_state == 1 && !risingEdge)
+  if (_state == 1)
   {
     if (millis() - _countOn >= _timeOn)
+    {
       _state = 0;
+      _countOff = millis();
+    }
   }
 
-  if (_state == 0 && !fallingEdge)
+  if (_state == 0)
   {
     if (millis() - _countOff >= _timeOff)
+    {
       _state = 1;
+      _countOn = millis();
+    }
   }
+
+  // Serial.print("STATE: ");
+  // Serial.print(_state);
+  // Serial.print(" | LAST STATE: ");
+  // Serial.print(_lastState);
+  // Serial.print(" | TON: ");
+  // Serial.print(millis() - _countOn);
+  // Serial.print(" / ");
+  // Serial.print(_timeOn);
+  // Serial.print(" | FOFF: ");
+  // Serial.print(millis() - _countOff);
+  // Serial.print(" / ");
+  // Serial.print(_timeOff);
+  // Serial.println("");
 
   // return the state
   return _state;
